73ad6a103e78938703ab3318d4cc6c048eff0768
Tim Mocking <timmocking@gmail.com>
2023-09-20 13:14:57 +0200

    Add ridge plot for marker distributions

--- R/Plots.R
+++ R/Plots.R
@@ -171,3 +171,48 @@ PlotPCA <- function(CF, featMethod, includeRef = "auto", flagMethod = NULL,
   }
   return(p)
 }
+
+
+PlotAnomaly <- function(CF, idx, channel, n = 500, includeRef = "auto"){
+  # Read the anomalous data
+  ff_anom <- ReadInput(CF$test_paths[idx], n = n)
+  df_anom <- data.frame(ff_anom@exprs[,channels], check.names=FALSE)
+  df_anom$index <- as.character(idx)
+  df_anom$category <- "Outlier"
+  
+  if (includeRef == "auto"){
+    includeRef <- "ref" %in% names(CF$features)
+  }
+  # Sample n other samples from test data
+  set.seed(42)
+  if (includeRef){
+    test_indices <- 1:length(CF$ref_paths)
+    background_label <- "Reference"
+  } else {
+    test_indices <- setdiff(1:length(CF$test_paths), idx)
+    background_label <- "Other test samples"
+  }
+  if (length(test_indices) > 20){
+    test_indices <- sample(test_indices, 20, replace = FALSE)
+  }
+  
+  test_data <- list()
+  for (test_idx in test_indices){
+    print(test_idx)
+    ff <- ReadInput(CF$test_paths[[test_idx]], n  = n)
+    df <- data.frame(ff@exprs[,channels], check.names=FALSE)
+    df$index <- as.character(test_idx)
+    df$category <- background_label
+    test_data[[test_idx]] <- df
+  }
+  test_data <- do.call("rbind", test_data)
+  all_data <- rbind(df_anom, test_data)
+  
+  ggplot2::ggplot(all_data, ggplot2::aes(x = .data[[channel]], y = .data[["index"]],
+                                         fill = .data[["category"]])) +
+    ggridges::geom_density_ridges(scale = 5) +
+    ggplot2::scale_fill_manual(values = c("Outlier" = "red", "Reference" = "blue",
+                                          "Other test samples" = "blue")) +
+    ggplot2::labs(y = "Index") +
+    ggridges::theme_ridges() # to make it pretty
+}
