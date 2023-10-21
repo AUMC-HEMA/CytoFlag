e88c2a80b1914809c2da7826df7f4fd5328d2678
Tim Mocking <timmocking@gmail.com>
2023-10-22 00:10:02 +0200

    Refractor CytoFlag.R

--- R/CytoFlag.R
+++ R/CytoFlag.R
@@ -14,42 +14,53 @@ CytoFlag <- function(){
   return(CF)
 }
 
-#' @export
-addLabels <- function(CF, labels, slot){
-  CF[["labels"]][[slot]] <- labels
-  return(CF)
-}
  
+#' Add additional annotations for test data 
+#' 
+#' @param CF CytoFlag object
+#' @param labels Vector of numeric or string labels
+#'
+#' @return CytoFlag object
+#'
+#' @seealso \code{\link{addReferencelabels}}
+#'
 #' @export
 addTestlabels <- function(CF, labels){
-  CF <- addLabels(CF, labels, "test")
+  CF[["labels"]][["test"]] <- labels
   return(CF)
 }
 
+
+#' Add additional annotations for reference data 
+#' 
+#' @param CF CytoFlag object
+#' @param labels Vector of numeric or string labels
+#'
+#' @return CytoFlag object
+#'
+#' @seealso \code{\link{addTestlabels}}
+#'
 #' @export
 addReferencelabels <- function(CF, labels){
-  CF <- addLabels(labels, "ref")
+  CF[["labels"]][["ref"]] <- labels
   return(CF)
 }
 
-#' @export
+
+#' @noRd
 processInput <- function(ff){
   ff <- PeacoQC::RemoveMargins(ff, channels)
-  ff <- flowCore::compensate(ff, ff@description$SPILL)
-  ff <- flowCore::transform(ff, flowCore::transformList(colnames(ff@description$SPILL), 
-                                                        flowCore::arcsinhTransform(a = 0, b = 1/150, c = 0)))
+  spill <- ff@description$SPILL
+  ff <- flowCore::compensate(ff, spill)
+  ff <- flowCore::transform(ff, flowCore::transformList(colnames(spill), 
+                                            flowCore::arcsinhTransform(a = 0, 
+                                                                       b = 1/150, 
+                                                                       c = 0)))
   return(ff)
 }
 
 
-#' Read, preprocess and downsample FCS files
-#'
-#' @param CF CytoFlag object
-#' @param path Location of FCS file
-#' @param n Total number of cells to read per file
-#'
-#' @return flowFrame
-#' @export
+#' @noRd
 readInput <- function(CF, path, n = NULL){
   set.seed(42)
   ff <- flowCore::read.FCS(path, which.lines = n)
@@ -58,178 +69,117 @@ readInput <- function(CF, path, n = NULL){
 }
 
 
-#' Add data to an initialized CytoFlag object
-#'
-#' @param CF CytoFlag object
-#' @param input List of FCS file paths
-#' @param slot Slot in the CytoFlag object to store data
-#' @param n Total number of cells to read per file (default = 1000)
-#'
-#' @return CytoFlag object
-#' @export
-addData <- function(CF, input, slot, n = 1000){
-  for (path in input){
-    ff <- readInput(CF, path, n)
-    CF[[slot]][[path]] <- data.frame(ff@exprs, check.names = FALSE)
-  }
-  return(CF)
-}
-
-
-#' Add reference data to initialized CytoFlag object
-#'
-#' @param CF CytoFlag object
-#' @param input List of FCS file paths
-#' @param read Whether to read FCS files already (default = FALSE)
-#' @param reload Whether to read ALL fcs files, including previously loaded (default = FALSE)
-#' @param aggSize How many cells to use in total for aggregate sample (default = 10000)
-#'
-#' @return CytoFlag object
-#' 
-#' @export
-addReferencedata <- function(CF, input, read = FALSE, reload = FALSE,
-                             aggSize = 10000){
+#' @noRd
+addData <- function(CF, input, pathSlot, dataSlot, read, 
+                    reload, aggSize){
   # Check if the paths are already stored in the CytoFlag object
-  if ("ref_paths" %in% names(CF) & !reload){
+  if (pathSlot %in% names(CF) & !reload){
     for (path in input){
-      if (!path %in% CF$ref_paths){
+      if (!path %in% CF[[pathSlot]]){
         message(paste("Concatenating additional file path", path))
-        CF$ref_paths <- c(CF$ref_paths, path)
+        CF[[pathSlot]] <- c(CF[[pathSlot]], path)
         if (read == TRUE){
-          CF <- addData(CF, path, "ref_data", CF$nAgg)
+          ff <- readInput(CF, path, CF$nAgg)
+          CF[[dataSlot]][[path]] <- data.frame(ff@exprs, check.names = FALSE)
+          }
+        }
+      }
+    } else {
+      CF[[pathSlot]] <- input
+      if (read == TRUE){
+        CF$aggSize <- aggSize
+        CF$nAgg <- round(aggSize / length(input))
+        for (path in input){
+          ff <- readInput(CF, path, CF$nAgg)
+          CF[[dataSlot]][[path]] <- data.frame(ff@exprs, check.names = FALSE)
         }
       }
     }
-  }
-  else {
-    CF$ref_paths <- input
-    if (read == TRUE){
-      CF$aggSize <- aggSize
-      CF$nAgg <- round(aggSize / length(input))
-      CF <- addData(CF, input, "ref_data", CF$nAgg)
-    }
-  }
   return(CF)
 }
 
 
-#' Add reference data to initialized CytoFlag object
+#' Add test data to CytoFlag object
 #'
 #' @param CF CytoFlag object
 #' @param input List of FCS file paths
-#' @param read Whether to read FCS files already (default = FALSE)
-#' @param reload Whether to read ALL fcs files, including previously loaded (default = FALSE)
-#' @param aggSize How many cells to use in total for aggregate sample (default = 10000)
+#' @param read Whether load subsampled data already (default = FALSE)
+#' @param reload Reload all data (default = FALSE)
+#' @param aggSize Size of aggregate sample (default = 10000 cells)
 #'
 #' @return CytoFlag object
+#'
+#' @seealso \code{\link{addReferencedata}}
 #' 
 #' @export
 addTestdata <- function(CF, input, read = FALSE, reload = FALSE,
                         aggSize = 10000){
-  # Check if the paths are already stored in the CytoFlag object
-  if ("test_paths" %in% names(CF) & !reload){
-    for (path in input){
-      if (!path %in% CF$test_paths){
-        message(paste("Concatenating additional file path", path))
-        CF$test_paths <- c(CF$test_paths, path)
-        if (read == TRUE){
-          CF <- addData(CF, path, "test_data", CF$nAgg)
-        }
-      }
-    }
-  }
-  else {
-    CF$test_paths <- input
-    if (read == TRUE){
-      CF$aggSize <- aggSize
-      CF$nAgg <- round(aggSize / length(input))
-      CF <- addData(CF, input, "test_data", CF$nAgg)
-    }
-  }
+  CF <- addData(CF, input, "test_paths", "test_data", read, reload, aggSize)
   return(CF)
 }
 
 
-#' Flag anomalies in CytoFlag object
+#' Add reference data to CytoFlag object
 #'
 #' @param CF CytoFlag object
-#' @param featMethod Feature generation method to use.
-#' Either one of: "summary", "EMD", "binning" or "fingerprint".
-#' @param flagStrat Which anomaly detection strategy to use ("outlier" or "novelty").
-#' By default, selects intended use based on availability of reference material.
-#' @param PCA Whether to reduce dimensionality to first two PCs. (default = FALSE)
+#' @param input List of FCS file paths
+#' @param read Whether load subsampled data already (default = FALSE)
+#' @param reload Reload all data (default = FALSE)
+#' @param aggSize Size of aggregate sample (default = 10000 cells)
+#'
+#' @return CytoFlag object
+#'
+#' @seealso \code{\link{addTestdata}}
+#' 
+#' @export
+addReferencedata <- function(CF, input, read = FALSE, reload = FALSE,
+                             aggSize = 10000){
+  CF <- addData(CF, input, "ref_paths", "ref_data", read, reload, aggSize)
+  return(CF)
+}
+
+
+#' Flag anomalies based on generated features 
+#'
+#' @param CF CytoFlag object
+#' @param featMethod Which features to use for anomaly detection
+#' @param flagStrat Which flaggins strategy to use ("outlier" or "novelty")
+#' @param PCA Reduce features to 2D PCA before flagging (default = FALSE)
 #'
 #' @return CytoFlag object
 #' 
+#' @seealso \code{\link{generateFeatures}}
+#'
 #' @export
-Flag <- function(CF, featMethod = NULL, flagStrat = "outlier",
-                 PCA = FALSE){
-  # Detect  flagging strategy if not supplied based on CytoFlag object
-  if (flagStrat == "auto"){
-    if ("test" %in% names(CF$features) & "ref" %in% names(CF$features)){
-      message("Detected reference and test data, flagging samples using novelty detection")
-      flagStrat <- "novelty"
-    }
-    else if ("test" %in% names(CF$features)){
-      message("Detected only test data, flagging samples using outlier detection")
-      flagStrat <- "outlier"
-    }
-    else {
-      stop("Did not detect generated features in CytoFlag object")
-    }
+Flag <- function(CF, featMethod, flagStrat, PCA = FALSE){
+  testFeatures <- CF$features$test[[featMethod]]
+  if (flagStrat == "novelty"){
+    refFeatures <- CF$features$ref[[featMethod]]
   }
-  
-  # Get the features
-  if (is.null(featMethod)){
-    testFeatures <- CF$features$test[[names(CF$features$test)[1]]]
-    if (flagStrat == "novelty"){
-      refFeatures <- CF$features$ref[[names(CF$features$ref)[1]]]
-    }
-  }
-  else{
-    testFeatures <- CF$features$test[[featMethod]]
-    if (flagStrat == "novelty"){
-      refFeatures <- CF$features$ref[[featMethod]]
-    }
-  }
-  
   if (PCA){
-    message("Reducing features to first two principal components")
     if (flagStrat == "outlier"){
       PCAOutput <- reduceDim(testFeatures, "outlier")
       testFeatures <- PCAOutput[["testFeatures"]]
     }
     if (flagStrat == "novelty"){
-      # Fit the PCA on the reference features before reducing the test features
+      # Fits the PCA on the reference features before test
       PCAOutput <- reduceDim(testFeatures, "novelty", refFeatures)
       testFeatures <- PCAOutput[["testFeatures"]]
       refFeatures <- PCAOutput[["refFeatures"]]
     }
   }
-  
   if (flagStrat == "novelty"){
     # Based on: https://bookdown.org/egarpor/NP-UC3M/kde-ii-mult.html
-    ref_scores <- ks::kde(x = refFeatures, eval.points = refFeatures)$estimate
-    test_scores <- ks::kde(x = refFeatures, eval.points = testFeatures)$estimate
-    threshold <- stats::quantile(ref_scores, 0.05)
-    novelties <- as.factor(ifelse(test_scores < threshold, TRUE, FALSE))
-    # n <- length(novelties[novelties == TRUE])
-    # perc <- (n / length(novelties)) * 100
-    # fmt_str <- sprintf("Found %d novelties (%.1f%%)", n, perc)
-    # message(fmt_str)
+    refScores <- ks::kde(x = refFeatures, eval.points = refFeatures)$estimate
+    testScores <- ks::kde(x = refFeatures, eval.points = testFeatures)$estimate
+    threshold <- stats::quantile(refScores, 0.05)
+    novelties <- as.factor(ifelse(testScores < threshold, TRUE, FALSE))
     CF$novelties[[featMethod]] <- novelties
-  }
-  
-  if (flagStrat == "outlier"){
+  } else if (flagStrat == "outlier"){
     forest <- isotree::isolation.forest(testFeatures, seed = 42)
     scores <- stats::predict(forest, testFeatures)
-    # Scores above 0.5 are most likely outliers according to the original paper
-    # This also the default behavior in the sklearn implementation
+    # Scores > 0.5 are most likely outliers according to the original paper
     outliers <- as.factor(ifelse(scores >= 0.5, TRUE, FALSE))
-    # n <- length(outliers[outliers == TRUE])
-    # perc <- (n / length(outliers)) * 100
-    # fmt_str <- sprintf("Found %d outliers (%.1f%%)", n, perc)
-    # message(fmt_str)
     CF$outliers[[featMethod]] <- outliers
   }
   return(CF)
